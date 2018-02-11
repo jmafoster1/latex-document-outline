2963c16c185957d089a3655b120fe1148824a102
Michael Foster <jmafoster1@gmail.com>
2018-02-11 10:56:04 +0000

    Finds main latex file

---
+++ spec/latex-document-outline-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LatexDocumentOutlineView from '../lib/latex-document-outline-view';
+
+describe('LatexDocumentOutlineView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
